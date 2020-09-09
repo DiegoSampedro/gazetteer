40e3fc3b95ac7db82f3bcf5aa755fccf59bf70a0
DiegoSampedro <diegosampedrovina@gmail.com>
2020-09-09 13:53:13 +0100

    5th commit

--- js/leaflet.js
+++ js/leaflet.js
@@ -12,9 +12,19 @@ if (navigator.geolocation) {
           iconUrl: './images/map-marker.png',
           iconSize: [38, 42],
           iconAnchor: [19, 30],
-          popupAnchor: [-3, -76]
+          popupAnchor: [2, -30]
           });
       var marker = L.marker([latit, longit], {icon: myIcon}).addTo(mymap);
+      var popupLocation1 = new L.LatLng(latit, longit);
+        var popupContent1 = 'You are here';
+
+        popup1 = new L.Popup();
+        popup1.setLatLng(popupLocation1);
+        popup1.setContent(popupContent1);
+
+        marker.bindPopup(popup1);
+
+        marker.openPopup();
 
        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
           attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -42,7 +52,8 @@ if (navigator.geolocation) {
                 $('#continent').html(result['geonames'][0]['continentName']);
                 $('#capital').html(result['geonames'][0]['capital']);
                 $('#population').html(result['geonames'][0]['population']);
-                $('#currency').html(result['results'][0]['currency']);
+                var lowerCode = (result['geonames'][0]['countryCode']).toLowerCase();
+                $('#flag').attr('src', 'https://www.countryflags.io/' + lowerCode + '/shiny/64.png');
                 $('#currencyCode').html(result['geonames'][0]['currencyCode']);
                 $('#exchange').html(result['rateData']['rates'][result['geonames'][0]['currencyCode']] + ' ' + result['geonames'][0]['currencyCode'] + ' / 1$');
                 $('#area').html(result['geonames'][0]['areaInSqKm']);
@@ -67,7 +78,8 @@ if (navigator.geolocation) {
     })
    }  
    )} else {
-       $(".info").html("This browser doesn't support geolocation.")
+       $("#country2").html("This browser doesn't support geolocation.");
+       $("#continent").html("Please select a country on the dropdown menu.");
    }
 
 
@@ -85,11 +97,47 @@ if (navigator.geolocation) {
                 console.log(result);
     
                 if (result.status.name == "ok") {
+
+                    mymap.setView([result['results'][0]['geometry']['lat'], result['results'][0]['geometry']['lng']], 5);
+                   // popupLocation1 = new L.LatLng(result['results'][0]['geometry']['lat'], result['results'][0]['geometry']['lng']);
+                   // popupContent1 = 'This is the capital';
+                   // var newLatLng = new L.LatLng(result['results'][0]['geometry']['lat'], result['results'][0]['geometry']['lng']);
+                   // marker.setLatLng(newLatLng); 
+
+                    /*var mymap = L.map('mapid').setView([result['results'][0]['geometry']['lat'], result['results'][0]['geometry']['lng']], 5);
+                    var myIcon = L.icon({
+                        iconUrl: './images/map-marker.png',
+                        iconSize: [38, 42],
+                        iconAnchor: [19, 30],
+                        popupAnchor: [2, -30]
+                        });
+                    var marker = L.marker([result['results'][0]['geometry']['lat'], result['results'][0]['geometry']['lng']], {icon: myIcon}).addTo(mymap);
+                    var popupLocation1 = new L.LatLng(result['results'][0]['geometry']['lat'], result['results'][0]['geometry']['lng']);
+                    var popupContent1 = 'This is the capital';
+
+                        popup1 = new L.Popup();
+                        popup1.setLatLng(popupLocation1);
+                        popup1.setContent(popupContent1);
+
+                        marker.bindPopup(popup1);
+
+                        marker.openPopup();
+
+                    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+                        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                        maxZoom: 18,
+                        id: 'mapbox/streets-v11',
+                        tileSize: 512,
+                        zoomOffset: -1,
+                        accessToken: 'pk.eyJ1IjoiZGllZ29zYW1wZWRybyIsImEiOiJja2VsZHcyMXgwbG1oMnJud2R2bTg4b2MwIn0.D6B-FvY03F2vrMHY59DSEw'
+                        }).addTo(mymap);*/
+
                     $('#country2').html(result['results'][0]['country']);
                     $('#continent').html(result['geonames'][0]['continentName']);
                     $('#capital').html(result['geonames'][0]['capital']);
                     $('#population').html(result['geonames'][0]['population']);
-                    $('#currency').html(result['results'][0]['currency']);
+                    var lowerCode = (result['geonames'][0]['countryCode']).toLowerCase();
+                    $('#flag').attr('src', 'https://www.countryflags.io/' + lowerCode + '/shiny/64.png');
                     $('#currencyCode').html(result['geonames'][0]['currencyCode']);
                     $('#exchange').html(result['rateData']['rates'][result['geonames'][0]['currencyCode']] + ' ' + result['geonames'][0]['currencyCode'] + ' / 1$');
                     $('#area').html(result['geonames'][0]['areaInSqKm']);
